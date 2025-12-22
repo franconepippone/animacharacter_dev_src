55a5f82706cce24f96c362ec1fa3f66d5ffcdc62
Pippone Francone <americo.cherubini2003@gmail.com>
2025-12-22 12:40:06 +0100

    initial commit
    the aim is making this a standalone repo that we use as a submodule in the other projects

---
+++ src/SerialDevice.cpp
@@ -0,0 +1,171 @@
+#include <Arduino.h>
+#include "SerialDevice.h"
+
+// ======================= DEBUG FUNCTIONS =======================
+
+bool _debug_blink_builtin(int times, int period) {
+    for (int i = 0; i < times; i++) {
+        digitalWrite(13, HIGH);
+        delay(period);
+        digitalWrite(13, LOW);
+        delay(period);
+    }
+}
+
+bool _debug_triggerIdent(SerialDevice* dev) {
+    _debug_blink_builtin(20, 20);
+    dev->requestPeername(1000);
+    _debug_blink_builtin(5, 200);
+    dev->sendPacket(dev->peerName);
+    _debug_blink_builtin(20, 20);
+    delay(100);
+}
+
+// ======================== DEFAULT CALLBACKS ========================
+
+bool _handleAuthRqst(SerialDevice* dev) {
+    dev->recvPacket(dev->peerName); // stores name string in the peerName variable 
+    dev->sendPacket(dev->deviceName, PACKID_IDENT_RESP);
+}
+
+bool _handlePing(SerialDevice* dev) {
+    // if respond flag is set to true, this is a ping request and we send a response
+    bool respond;
+    dev->recvPacket(respond);
+    if (respond) dev->sendPacket(false, PACKID_PING);
+}
+
+
+// ======================= SERIAL DEVICE IMPLEMENTATION =======================
+
+SerialDevice::SerialDevice(HardwareSerial& serial, const char* name)
+    : ser(&serial)
+{
+    strncpy(deviceName, name, sizeof(deviceName));
+    on(PACKID_IDENT_RQST, _handleAuthRqst);
+    on(PACKID_PING, _handlePing);
+    on(PACKID_DEBUG_TRIGGER_IDENT_RQST, _debug_triggerIdent);
+    txf.begin(*ser);
+}
+
+void SerialDevice::begin(unsigned long baud) {
+    ser->begin(baud);
+}
+
+void SerialDevice::on(uint8_t packetId, PacketHandler handler) {
+    if (packetId < MAX_HANDLERS) {
+        handlers[packetId] = handler;
+    }
+}
+
+void SerialDevice::onAny(WidePacketHandler handler) {
+    baseHandler = handler;
+}
+
+uint8_t SerialDevice::poll() {
+    uint8_t amount = available();
+    if (amount) {
+        const uint8_t latestPackId = txf.currentPacketID();
+        if (handlers[latestPackId]) {
+            handlers[latestPackId](this);
+            return 0;
+        } else if (baseHandler) {
+            baseHandler(latestPackId, this);
+            return 0;
+        } else return amount;
+    }
+    return 0;
+}
+
+uint8_t SerialDevice::available() {
+    return txf.available();
+}
+
+bool SerialDevice::requestPeername(uint32_t timeoutMs) {
+    sendPacket(deviceName, PACKID_IDENT_RQST);
+
+    while (1) {
+        timeoutMs = waitPacket(timeoutMs);
+        if (!timeoutMs) return false;
+
+        const uint8_t packId = txf.currentPacketID();
+        if (packId == PACKID_IDENT_RESP) {
+            recvPacket(peerName);
+            return true;
+        }
+    }
+}
+
+uint64_t SerialDevice::waitPacket(uint64_t timeoutMs) {
+    auto start = millis();
+    uint64_t remaining = timeoutMs;
+    while (remaining) {
+        auto elapsed = millis() - start;
+        remaining = max(0, timeoutMs - elapsed);
+        if (available()) return remaining;
+    }
+    return 0;
+}
+
+// ======================= SEND API =======================
+
+uint8_t SerialDevice::sendBytes(const byte *buffer, size_t size, uint8_t packId) {
+    size = min(size, sizeof(txf.packet.txBuff));
+    memcpy(txf.packet.txBuff, buffer, size);
+    return txf.sendData(size, packId);
+}
+
+template <size_t N>
+uint8_t SerialDevice::sendPacket(char (&str)[N], uint8_t packId) {
+    size_t len = strnlen(str, N);
+    return sendBytes((byte*)str, len, packId);
+}
+
+template <typename T>
+uint8_t SerialDevice::sendPacket(const T& obj, uint8_t packId) {
+    uint16_t size = txf.txObj(obj);
+    return txf.sendData(size, packId);
+}
+
+uint8_t SerialDevice::sendPacket(const char* str, uint8_t packId) {
+    uint16_t size = strlen(str);
+    return sendBytes((byte*)str, size, packId);
+}
+
+// ======================= RECEIVE API =======================
+
+template <typename T>
+size_t SerialDevice::recvPacket(T& obj) {
+    return txf.rxObj(obj);
+}
+
+size_t SerialDevice::recvBytes(byte* dst, size_t cap) {
+    size_t n = min(cap, (size_t)txf.bytesRead);
+    memcpy(dst, txf.packet.rxBuff, n);
+    return n;
+}
+
+void SerialDevice::recvPacket(char* dst, size_t cap) {
+    if (!dst || !cap) return;
+
+    size_t n = strnlen((char*)txf.packet.rxBuff, txf.bytesRead);
+    if (n >= cap) n = cap - 1;
+
+    memcpy(dst, txf.packet.rxBuff, n);
+    dst[n] = '\0';
+}
+
+template <size_t N>
+void SerialDevice::recvPacket(char (&dst)[N]) {
+    size_t n = strnlen((char*)txf.packet.rxBuff, txf.bytesRead);
+    if (n >= N) n = N - 1;
+
+    memcpy(dst, txf.packet.rxBuff, n);
+    dst[n] = '\0';
+}
+
+// ======================= RESET =======================
+
+void SerialDevice::reset() {
+    txf.reset();
+}
---
+++ src/SerialDevice.h
@@ -0,0 +1,124 @@
+#pragma once
+#include <Arduino.h>
+#include "SerialTransfer.h"
+
+// ======================= STRUCTS / TYPEDEFS =======================
+
+class SerialDevice;
+
+typedef bool (*PacketHandler)(SerialDevice*);
+typedef bool (*WidePacketHandler)(uint8_t, SerialDevice*);
+typedef void (*LargePacketHandler)(byte *buffer, size_t size, uint8_t pack_id);
+
+// ======================= DEFAULT PACKET IDs =======================
+
+// default packets IDs (reserved 200-220)
+#define PACKID_IDENT_RQST 200
+#define PACKID_IDENT_RESP 201
+#define PACKID_DIAGNOSTIC 202
+#define PACKID_PING 203
+// packets for large transfer
+#define PACKID_LARGETX_BEGIN 204
+#define PACKID_LARGETX_CHUNK 205
+#define PACKID_LARGETX_ACK 206
+#define PACKID_LARGETX_END
+
+// debug hooks
+#define PACKID_DEBUG_TRIGGER_IDENT_RQST 210
+
+// ======================= DEBUG FUNCTIONS =======================
+
+// Blinks the built-in LED a number of times
+bool _debug_blink_builtin(int times, int period = 100);
+
+// used to trigger device to perform a peername request (used for debugging)
+bool _debug_triggerIdent(SerialDevice* dev);
+
+// ======================= DEFAULT PACK HANDLERS =======================
+// Handler for ping
+bool handlePing(SerialDevice* dev);
+
+// Handler for device identification protocol
+bool handleAuthRqst(SerialDevice* dev);
+
+// ======================= SERIAL DEVICE CLASS =======================
+
+/// @brief Allows interaction over a serial stream in a packet oriented way. Uses the SerialTransfer object from
+/// SerialTransfer library (features packetized serial transactions up to payloads of 254 bytes,
+/// using of COBS and CRC). This class has better support for callback-based usage and device identification, and aims
+/// at slightly improving ease-of-use. 
+/// SerialTransfer repository: https://github.com/PowerBroker2/SerialTransfer
+class SerialDevice {
+private:
+
+    static const int MAX_HANDLERS = 256;  // category IDs are bytes
+    PacketHandler handlers[MAX_HANDLERS] = {nullptr};
+    WidePacketHandler baseHandler = nullptr;
+    LargePacketHandler largeRecvHandler = nullptr;
+
+public:
+    SerialTransfer txf;
+    char peerName[32] = "";
+    char deviceName[32]; // used for authentication with the other device.
+    HardwareSerial* ser;
+    
+    SerialDevice(HardwareSerial& serial, const char* name = "dev_default");
+    
+    // begins serial communication
+    void begin(unsigned long baud);
+
+    // Registers a handler function for a specific packet id
+    void on(uint8_t packetId, PacketHandler handler);
+
+    // Registers a fallback handler function for any received packet
+    void onAny(WidePacketHandler handler);
+
+    // Registers a handler function for any received packet from Large Transfers
+    void onLargeRecv(LargePacketHandler handler);
+    
+    // polls the serial stream and calls the appropriate handler if a full packet has been received.
+    // If no handler was called, returns the amount of payload bytes read from the buffer
+    uint8_t poll();
+
+    // Sends an identification request to peer; returns true if peer responded
+    bool requestPeername(uint32_t timeoutMs);
+
+    // Blocks execution until a packet is received, returns remaining time until timeout
+    uint64_t waitPacket(uint64_t timeoutMs);
+
+    // Parses incoming serial data. Returns amount of payload bytes if successful
+    // NOTE: This bypasses handlers! Use this if you want to process packets manually,
+    // otherwise, use poll()
+    inline uint8_t available();
+
+    // ======================= SEND API =======================
+
+    uint8_t sendBytes(const byte *buffer, size_t size, uint8_t packId = 0);
+
+    template <size_t N>
+    uint8_t sendPacket(char (&str)[N], uint8_t packId = 0);
+
+    template <typename T>
+    uint8_t sendPacket(const T& obj, uint8_t packId = 0);
+
+    uint8_t sendPacket(const char* str, uint8_t packId = 0);
+
+    // ======================= LARGE TRANSFER PROTOCOL =======================
+
+    auto sendLarge(const byte *buffer, size_t size, uint8_t packId = 0);
+
+    // ======================= RECEIVE API =======================
+
+    template <typename T>
+    size_t recvPacket(T& obj);
+
+    size_t recvBytes(byte* dst, size_t cap);
+
+    void recvPacket(char* dst, size_t cap);
+
+    template <size_t N>
+    void recvPacket(char (&dst)[N]);
+
+    // clears buffers and resets the device
+    void reset();
+};
