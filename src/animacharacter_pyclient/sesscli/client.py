e5eca6d1019a19caa926a349444bf2c04396ddbb
Americo <americo.cherubini2003@gmail.com>
2026-01-30 22:34:46 +0100

    refactor: enhance session handling and update dependencies
    - Introduced SessionData dataclass for better session management.
    - Updated attempt_session_http_rqst method to handle API responses more robustly.
    - Added pynng dependency to pyproject.toml and uv.lock for improved messaging capabilities.
    
    added a working echo example right after session creation to prove that nng channel is working

--- src/animacharacter_pyclient/sesscli/client.py
+++ src/animacharacter_pyclient/sesscli/client.py
@@ -1,22 +1,58 @@
 import requests
+from dataclasses import dataclass
+import pynng
 import time
+import logging
+
+@dataclass  
+class SessionData:
+    success: bool = False
+    msg: str = ""
+    nng_port: int = -1
+    udp_port: int = -1
+    udp_secret_key: str = ""
+    token: str = ""
 
 
 class ACRemoteClient:
     def __init__(self, ip: str) -> None:
         self.ip = ip
-        self.base_url = f"http://{ip}:8000/auth"
+        self.base_url = f"http://{self.ip}:8000/auth"
+        self.session_data: SessionData = SessionData(success=False)
 
-    def create_session(self, secret_key: str, timeout: int = 10) -> bool:
+    def attempt_session_http_rqst(self, secret_key: str, timeout: int = 10) -> bool:
         try:
-            resp = requests.post(f"http://{self.ip}:8000/auth", json={"key": secret_key}, timeout=timeout)
+            resp = requests.post(f"http://{self.ip}:8000/auth", json={"apikey": secret_key}, timeout=timeout)
         except requests.exceptions.RequestException as e:
-            print(e)
             return False
-        else:
-            print(resp.status_code)
+        
+        if resp.status_code != 200:
+            return False
 
-            respjson = resp.json()
-            print(resp.json())
+        try:
+            respjson: dict = resp.json()
+        except requests.exceptions.JSONDecodeError:
+            return False
+        
+        session_data = SessionData(
+            success=respjson.get("success", False),
+            msg=respjson.get("msg", ""),
+            nng_port=respjson.get("nng_port", -1),
+            udp_port=respjson.get("udp_port", -1),
+            udp_secret_key=respjson.get("udp_secret_key", ""),
+            token=respjson.get("token", "")
+        )
+        self.session_data = session_data
 
-        return respjson.get("success", False)
+        print(self.session_data)
+        return self.session_data.success
+
+
+if __name__ == "__main__":
+    client =    ACRemoteClient("127.0.0.1")
+    client.attempt_session_http_rqst("supersecret-client-key")
+
+    with pynng.Pair0(dial=f"tcp://127.0.0.1:{client.session_data.nng_port}") as pynng_sock:
+        pynng_sock.send(b"Hello from client")
+        data = pynng_sock.recv(True)
+        print(f"Received from server: {data}")
