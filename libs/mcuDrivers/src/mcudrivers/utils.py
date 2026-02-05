31f95ccf04bddbf6fb06093bbd05a5fe2aa84e60
Americo <americo.cherubini2003@gmail.com>
2026-02-05 02:36:10 +0100

    added utilities function to the head driver

--- libs/mcuDrivers/src/mcudrivers/utils.py
+++ libs/mcuDrivers/src/mcudrivers/utils.py
@@ -1,8 +1,27 @@
 # Utility functions
 
+def tobyte(x: int) -> int:
+    """clamps integer to byte range"""
+    return min(max(x, 0), 255)
+
 def remap(x: float, to_max: int = 255):
     """Remaps x from range [0, 1] to range [0, to_max]"""
     return min(max(x, 0.0), 1.0) * to_max
+
+def clamp(x: float, lo: float, hi: float) -> float:
+    if x < lo:
+        return lo
+    if x > hi:
+        return hi
+    return x
+
+def topwm8(x: float) -> int:
+    """Maps float in range [0, 1] to integer in [0, 255]"""
+    return min(max(int(x) * 255, 0), 1)
+
+def topwm12(x: float) -> int:
+    """Maps float in range [0, 1] to integer in [0, 4095]"""
+    return min(max(int(x) * 4095, 0), 1)
 
 def nrm(x: float) -> float:
     return (x+1.0)*0.5
