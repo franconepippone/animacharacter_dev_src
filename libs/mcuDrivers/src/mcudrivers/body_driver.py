eefdaca0191e0d9c95387f15ed89a69ce0ea44ca
Pippone Francone <americo.cherubini2003@gmail.com>
2026-04-20 01:21:27 +0200

    base driver working
    syncronized constants with the c++ side and the driver is functional, missing some useful api features and logging handling

--- libs/mcuDrivers/src/mcudrivers/body_driver.py
+++ libs/mcuDrivers/src/mcudrivers/body_driver.py
@@ -1,6 +1,8 @@
 import time
 from pySerialDevice import SerialDevice
 from mcudrivers.constants import *
+import struct
+
 
 class BodyMcuDriver:
 
@@ -24,9 +26,18 @@ class BodyMcuDriver:
             self.CFG.BAUDRATE
         )
 
+        self.packer = struct.Struct("<hhh")
+        self.payload_buffer = bytearray(6)
+
+        # state
+
+        self.left_mot: int = 0
+        self.right_mot: int = 0
+        self.rotation_mot: int = 0
+
     def begin(self) -> bool:
         """
-        Initialize hardware connection and synchronize with MCU.
+        Initialize hardware connection and put hardware in a ready-to-operate state.
         
         Returns:
             True if initialization successful, False otherwise.
@@ -38,24 +49,33 @@ class BodyMcuDriver:
             return False
 
         time.sleep(2) # needed for arduino nano
+        
         name = self.dev.request_peername(5)
         if name != self.CFG.PEER_NAME:
             print(f"Serial device not match expected name: got {name} instead of {self.CFG.PEER_NAME}")
             return False
 
-        self.dev.send_object(PSP_INIT_HARDWARE, PACK_ID_CONTROL)
-        #if not self.dev.wait_packet(5):
-        #    print(f"Failed to received ack packet after hardware init request")
-        #    return False
-
-        time.sleep(.5)
-        self.dev.send_object(PSP_BEGIN_ALL, PACK_ID_CONTROL)
-        time.sleep(.5)
-
-        #if not self.flush():
-        #    print("Could not flush initial hardware state")
-        #    return False
+        self.dev.send_object(INIT_HARDWARE, PSP_PACKID_CONTROL_FLAGS)
+        if not (p := self.dev.wait_packet(5)):
+            print(f"Failed to received ack packet after hardware init request")
+            return False
         
+        if not (p.id == PSP_PACKID_DIAGNOSTICS and p.data == DGN_HARDWARE_INIT_OK):
+            print(f"Failed to initialize hardware")
+            return False 
+
+        time.sleep(.5)
+        self.dev.send_object(BEGIN_ALL, PSP_PACKID_CONTROL_FLAGS)
+        time.sleep(.5)
+
+        if not (p := self.dev.wait_packet(5)):
+            print("Timed out")
+            return False
+            
+        if not (p.id == PSP_PACKID_DIAGNOSTICS and p.data == DGN_BEGINALL_OK):
+            print(f"Failed to startup hardware (beginall failed)")
+            return False
+
         print("Hardware initialized.")
         self.initialized = True
         return True
@@ -64,29 +84,40 @@ class BodyMcuDriver:
         """Deinitialize hardware and close connection."""
         if not self.initialized: return True # dont if we are still deinitialized
 
-        if not self.dev.send_object(PSP_DEINIT, PACK_ID_CONTROL):
+        if not self.dev.send_object(DEINIT_HARDWARE, PSP_PACKID_CONTROL_FLAGS):
             print("Could not send deinit request to serial device, closing anyway...")
+            return False
+        if not (p := self.dev.wait_packet(5)):
+            print("Deinit ack timed out")
+            return False
+        if not (p.id == PSP_PACKID_DIAGNOSTICS and p.data == DGN_HARDWARE_DEINIT_ACK):
+            print("Invalid deinit ack")
+            return False
+         
         self.dev.close()
         self.initialized = False
+        print("deinit ok!")
         return True
 
     def isinit(self) -> bool:
         return self.initialized
+
+
+    def flush(self) -> bool:
+        self.packer.pack_into(self.payload_buffer, 0, self.right_mot, self.left_mot, self.rotation_mot)
+        return self.dev.send_object(self.payload_buffer, PSP_PACKID_MOTION)
+
     
 
 
-#driver = BodyMcuDriver('COM3')
-#driver.begin()
+driver = BodyMcuDriver('COM3')
+driver.begin()
 
-dev = SerialDevice("peppe", 'COM3')
-dev.open(1)
-time.sleep(2)
+import random
+import math
+for _ in range(100):
+    driver.rotation_mot = random.randint(-5000, 5000)
+    driver.flush()
+    time.sleep(.2)
 
-name = dev.request_peername(5)
-print(name)
-
-dev.send_object(PSP_INIT_HARDWARE, PACK_ID_CONTROL)
-
-while True:
-    name = dev.request_peername(5)
-    print(name)
+driver.deinit()
--- libs/mcuDrivers/src/mcudrivers/constants.py
+++ libs/mcuDrivers/src/mcudrivers/constants.py
@@ -1,10 +1,22 @@
-# these are the same for all animacharacter embedded devices
-PSP_INIT_HARDWARE = bytes([1])
-PSP_DEINIT = bytes([2])
-PSP_BEGIN_ALL = bytes([4])
+# SerialDevice packet ids (0-199, 231-255)   
+PSP_PACKID_MOTION = 1
+PSP_PACKID_LEDS = 2
+PSP_PACKID_PARAMETERS = 3
+PSP_PACKID_CONTROL_FLAGS = 4 # hardware related (start, stop, reset)
+PSP_PACKID_DIAGNOSTICS = 5
 
 
-#PACK IDS
-PACK_ID_CONTROL = 0x04
+#### = custom diagnostic error codes (1 byte)
+DGN_ERR_HARDWARE_INIT_FAIL = bytes([0x00]) # error during hardware initialization
+DGN_HARDWARE_INIT_OK = bytes([0x01])
+DGN_BEGINALL_OK = bytes([0x02])
+DGN_HARDWARE_DEINIT_ACK = bytes([0x03])
+# = ----- Body only
+DGN_WRN_MOTOR_R = bytes([0x04])
+DGN_WRN_MOTOR_L = bytes([0x05])
 
 
+# = lifecycle request flag bitmasks
+INIT_HARDWARE = bytes([0x01])
+BEGIN_ALL = bytes([0x02])
+DEINIT_HARDWARE = bytes([0x04])
