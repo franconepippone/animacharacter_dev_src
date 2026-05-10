feae45d2d1241031cd4abffc1c424637afb45fea
Americo <americo.cherubini2003@gmail.com>
2026-05-10 22:01:25 +0200

    moved main launch of session management nodes inside session manager package

--- ros2_ws/src/session_mng/launch/main.launch.py
+++ ros2_ws/src/session_mng/launch/main.launch.py
@@ -1,12 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from ament_index_python.packages import get_package_share_directory
-import os
-
 def generate_launch_description():
-
-    pkg_share = get_package_share_directory('orchestrator')
 
     return LaunchDescription([
         Node(
