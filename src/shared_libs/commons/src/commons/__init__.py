e092bef8e0798d136ed84d4e52a9c0e55a8a6166
Americo <americo.cherubini2003@gmail.com>
2026-05-11 22:33:48 +0200

    added vector based decoding of motionframes

--- src/shared_libs/commons/src/commons/__init__.py
+++ src/shared_libs/commons/src/commons/__init__.py
@@ -1,1 +1,10 @@
+"""
+Module containing all shared definitions, configurations and utilties between client and server.  
+Provides unique source of truth for shared data. 
+
+Version mistmatch of this package in client and server can lead (does lead) to incompatibilities. The installed versions
+must match to ensure compatibilty.
+"""
+
+
 from .utils import ActTableEntry
--- src/shared_libs/commons/src/commons/network/udp_motionframe_schema.py
+++ src/shared_libs/commons/src/commons/network/udp_motionframe_schema.py
@@ -1,9 +1,46 @@
 from typing import Tuple
 from collections.abc import Sequence
 import struct
+from array import array
+
+# ===========================================================
+#                     MOTIONFRAME PACKET
+# ===========================================================
+# 
+# Each Motionframe packet is a contiguous sequence of records.
+# Every record has a fixed binary layout:
+# 
+#     +---------+---------------------------+
+#     | 2 bytes |        4 bytes            |
+#     +---------+---------------------------+
+#     |   ID    |        VALUE (float32)    |
+#     +---------+---------------------------+
+# 
+# Record size: 6 bytes
+# 
+# Packet layout (N records):
+# 
+#     +---------+---------+---------+---------+-----+
+#     | Rec 0   | Rec 1   | Rec 2   | Rec 3   | ... |
+#     +---------+---------+---------+---------+-----+
+#       0..5      6..11     12..17    18..23   ...
+# 
+# Where each record is:
+# 
+#     Offset +0 : uint16  → actuator ID
+#     Offset +2 : float32 → actuator value
+# 
+# Example (N = 3):
+# 
+#     Byte index:
+#         0   1   2   3   4   5   6   7   8   9  10  11  ...
+#         |---ID0---|----VALUE0----|---ID1---|----VALUE1----| ...
+# 
+# ===========================================================
 
 
-packer = struct.Struct("Bf")
+packer = struct.Struct("Hf")
+
 
 def encode_motionframe_packet(motionframe: Sequence[Tuple[int, float]]) -> bytearray:
     unit_size = packer.size
@@ -29,3 +66,21 @@ def decode_motionframe_packet(data: bytes | bytearray) -> list[tuple[int, float]
         offset += unit_size
 
     return out
+
+def decode_motionframe_packet_into_arrays(data: bytes | bytearray):
+    # faster ros2 native implementation
+    unpack_from = packer.unpack_from
+    unit_size = packer.size
+
+    n = len(data) // unit_size
+    ids = array('H', (0,)) * n      # preallocate
+    vals = array('f', (0.0,)) * n   # preallocate
+
+    offset = 0
+    for i in range(n):
+        id_val, float_val = unpack_from(data, offset)
+        ids[i] = id_val
+        vals[i] = float_val
+        offset += unit_size
+
+    return ids, vals
